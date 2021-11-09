d27246d0d9250b58567d3cfb639005303f7861ee
Pham Van Khanh <khanhphamvan1910@gmail.com>
2021-11-09 10:00:01 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -2,10 +2,11 @@ import request from "request";
 
 require('dotenv').config();
 
-const page_ACCESS_TOKEN=process.env.PAGE_ACCESS_TOKEN;
+const page_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const IMAGE_GET_STARTED = 'https://www.mentalhealthfirstaid.org/wp-content/uploads/2018/03/Jacksonville-Hospital-Collaborative-1.jpg';
 
-let callSendAPI = (response, sender_psid) =>{
-     // Construct the message body
+let callSendAPI = (response, sender_psid) => {
+  // Construct the message body
   let request_body = {
     "recipient": {
       "id": sender_psid
@@ -25,24 +26,60 @@ let callSendAPI = (response, sender_psid) =>{
     } else {
       console.error("Unable to send message:" + err);
     }
-  }); 
+  });
 }
 
 
-    
 
-let handleGetStarted = (sender_psid) =>{
-    return new Promise(async (resolve, reject) =>{
-        try{
-            let response = { "text": "Xin chào bạn đến với trang web của mình" }
-            await callSendAPI(response, sender_psid);
-            resolve('done');
-        }catch(e){
-            reject(e);
-        }
-    })
+
+let handleGetStarted = (sender_psid) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let response = { "text": "Xin chào bạn đến với trang web của mình" }
+      let response2 = sendGetStartedTemplate();
+      //send text messenge
+      await callSendAPI(response, sender_psid);
+
+      //send generic template messenge
+      await callSendAPI(response2, sender_psid);
+
+
+      resolve('done');
+    } catch (e) {
+      reject(e);
+    }
+  })
+}
+
+let sendGetStartedTemplate = () => {
+  let response = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "Xin chào,chúng tôi sẽ hỗ trợ bạn ngay.",
+          "subtitle": "bạn muốn chúng tôi hỗ trợ dịch vụ nào?",
+          "image_url": IMAGE_GET_STARTED,
+          "buttons": [
+            {
+              "type": "postback",
+              "title": "Đặt lịch",
+              "payload": "BOOK",
+            },
+            {
+              "type": "postback",
+              "title": "Tư vấn",
+              "payload": "ADVISE",
+            }
+          ],
+        }]
+      }
+    }
+  }
+  return response;
 }
 
 module.exports = {
-    handleGetStarted: handleGetStarted
+  handleGetStarted: handleGetStarted
 }
